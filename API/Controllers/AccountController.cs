51f2422ebf8b60ecebb6891880d4e3b46de2c6e0
Tobi <layantobi@gmail.com>
2025-07-28 16:24:01 +0100

    Paging, sorting and filtering

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -34,7 +34,8 @@ public class AccountController(DataContext context,
         {
             UserName = user.UserName,
             Token = tokenService.CreateToken(user),
-            KnownAs = user.KnownAs
+            KnownAs = user.KnownAs,
+            Gender = user.Gender
         };
 
     }
@@ -61,6 +62,7 @@ public class AccountController(DataContext context,
         {
             UserName = user.UserName,
             KnownAs = user.KnownAs,
+            Gender = user.Gender,
             Token = tokenService.CreateToken(user),
             PhotoUrl = user.Photos.FirstOrDefault(x => x.IsMain)?.Url
         };
